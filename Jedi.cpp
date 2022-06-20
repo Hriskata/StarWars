f0c77c14c92046ad0bb4a6fac1f453b5dfe2c987
Hriskata <105062473+Hriskata@users.noreply.github.com>
2022-06-20 23:56:45 +0300

    Update Jedi.cpp

--- Jedi.cpp
+++ Jedi.cpp
@@ -23,8 +23,22 @@ std::string Jedi::get_Name_of_jedi() const
 	return name;
 }
 
-jedi_rank Jedi::get_Rank() const
+jedi_rank Jedi::get_Rank(std::ostream& os) const
 {
+	/*
+	switch (rank)
+	{
+	case YOUNGLING: std::cout << "YOUNGLING";    break;
+	case INITIAT: os << "INITIAT"; break;
+	case PADAWAN: os << "PADAWAN";  break;
+	case KNIGHT_ASPIRANT: os << "KNIGHT_ASPIRANT";   break;
+	case KNIGHT: os << "KNIGHT";   break;
+	case MASTER: os << "MASTER";   break;
+	case BATTLE_MASTER: os << "BATTLE_MASTER";   break;
+	case GRAND_MASTER: os << "GRAND_MASTER";   break;
+	}
+	return rank;
+	*/
 	return rank;
 }
 
@@ -43,6 +57,11 @@ double Jedi::get_Strength() const
 	return strength;
 }
 
+void Jedi::set_Strenth(double x)
+{
+	strength = strength * x;
+}
+
 void Jedi::print(std::ostream& os) const
 {
 	os << "Jedi's name : " << name << std::endl
@@ -51,17 +70,51 @@ void Jedi::print(std::ostream& os) const
 		<< "Jedi's saber color : " << saber_color << std::endl
 		<< "Jedi's strength : " << strength << std::endl;
 }
-/*
+
 void Jedi::rank_up(Jedi& j)
 {
-	j.rank = j.get_Rank() + 1;
+	if (j.get_Rank() == 0)
+		j.rank = jedi_rank::INITIAT;
+	else if (j.get_Rank() == 1)
+		j.rank = jedi_rank::PADAWAN;
+	else if (j.get_Rank() == 2)
+		j.rank = jedi_rank::KNIGHT_ASPIRANT;
+	else if (j.get_Rank() == 3)
+		j.rank = jedi_rank::KNIGHT;
+	else if (j.get_Rank() == 4)
+		j.rank = jedi_rank::MASTER;
+	else if (j.get_Rank() == 5)
+		j.rank = jedi_rank::BATTLE_MASTER;
+	else if (j.get_Rank() == 6)
+		j.rank = jedi_rank::GRAND_MASTER;
+	else std::cout << "This Jedi is already the highest rank!"<<std::endl;
+
 }
-*/
+
+void Jedi::rank_down(Jedi& j)
+{
+	if (j.get_Rank() == 1)
+		j.rank = jedi_rank::YOUNGLING;
+	else if (j.get_Rank() == 2)
+		j.rank = jedi_rank::INITIAT;
+	else if (j.get_Rank() == 3)
+		j.rank = jedi_rank::PADAWAN;
+	else if (j.get_Rank() == 4)
+		j.rank = jedi_rank::KNIGHT_ASPIRANT;
+	else if (j.get_Rank() == 5)
+		j.rank = jedi_rank::KNIGHT;
+	else if (j.get_Rank() == 6)
+		j.rank = jedi_rank::MASTER;
+	else if (j.get_Rank() == 7)
+		j.rank = jedi_rank::BATTLE_MASTER;
+	else std::cout<<"This Jedi is already the lowest rank!"<<std::endl;
+}
+
 std::ostream& operator<<(std::ostream& out, const Jedi& jedi)
 {
 	return out << "Jedi's name : " << jedi.get_Name_of_jedi() << std::endl
 		<< "Jedi's rank : " << jedi.get_Rank() << std::endl
 		<< "Jedi's age : " << jedi.get_Age() << std::endl
 		<< "Jedi's saber color : " << jedi.get_Saber_color() << std::endl
-		<< "Jedi's strength : " << jedi.get_Strength() << std::endl;
+		<< "Jedi's strength : " << jedi.get_Strength() << std::endl << std::endl;
 }
