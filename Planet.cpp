6ac71ae948fbea56f70cf4e20ba5c049b9243a0e
Hriskata <105062473+Hriskata@users.noreply.github.com>
2022-06-20 23:56:23 +0300

    Update Planet.cpp

--- Planet.cpp
+++ Planet.cpp
@@ -17,6 +17,16 @@ std::string Planet::get_Name_of_planet() const
 	return name;
 }
 
+size_t Planet::get_Population_size() const
+{
+	return population.size();
+}
+
+std::vector<Jedi> Planet::get_Vector() const
+{
+	return population;
+}
+
 void Planet::add_Jedi(const Jedi& j)
 {
 	for (int i = 0; i < population.size(); i++)
@@ -32,13 +42,87 @@ void Planet::remove_Jedi(const Jedi& j)
 	for (int i = 0; i < population.size(); i++)
 	{
 		if (j.get_Name_of_jedi() == population[i].get_Name_of_jedi())
-			population.pop_back();
+			population.erase(population.begin() + i);
 	}
 }
 
-void Planet::print_Planet_population(std::ostream& os) const
+void Planet::stronger_Jedi()
+{
+	double strongest_power = 0;
+	for (int i = 0; i < population.size(); i++)
+	{
+		if (population[i].get_Strength() > strongest_power)
+			strongest_power = population[i].get_Strength();
+	}		
+	for (int i = 0; i < population.size(); i++)
+	{
+		if (strongest_power == population[i].get_Strength())
+			population[i].print();
+	}
+}
+
+void Planet::youngest_Jedi()
+{
+	int youngest = INT_MAX;
+	int counter = 0;
+	for (int i = 0; i < population.size(); i++)
+	{
+		if (population[i].get_Age() <= youngest)
+		{
+			youngest = population[i].get_Age();
+			counter++;
+		}
+	}
+	if (counter == 0)
+		throw std::exception("There is no population!");
+
+	if (counter > 1)
+	{
+		for (int i = 0; i < population.size(); i++)
+		{
+			if (youngest == population[i].get_Age())
+			{
+				for (int j = i + 1; j < population.size(); j++)
+				{
+					if (youngest == population[j].get_Age() && (population[i].get_Name_of_jedi() > population[j].get_Name_of_jedi()))
+					{
+						std::swap(population[i], population[j]);
+					}
+				}
+			}
+		}
+	}
+	
+	for (int i = 0; i < population.size(); i++)
+	{
+		if (youngest == population[i].get_Age())
+			population[i].print();
+	}
+}
+
+void Planet::print_Planet_name(std::ostream& os)
 {
 	os << "Planet's name : " << name << std::endl;
+}
+
+void Planet::print_Planet_population(std::ostream& os)
+{
+	os << "Planet's name : " << name << std::endl;
+	for (int i = 0; i < population.size(); i++)
+	{
+		for (int j = i + 1; j < population.size(); j++)
+		{
+			if (population[i].get_Rank() > population[j].get_Rank())
+			{
+				std::swap(population[i], population[j]);
+			}
+			if (population[i].get_Rank() == population[j].get_Rank() && (population[i].get_Name_of_jedi() > population[j].get_Name_of_jedi()))
+			{
+				std::swap(population[i], population[j]);
+			}
+		}
+	}
+
 	for (int i = 0; i < population.size(); i++)
 	{
 		os << population[i] << std::endl;
